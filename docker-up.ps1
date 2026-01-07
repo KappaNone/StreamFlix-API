514521a3bd4f90e6c5be42d0ca55a9ef433e6633
arad5469326 <aradkashefsch@gmail.com>
2026-01-08 00:03:28 +0100

    test

---
+++ docker-up.ps1
@@ -0,0 +1,2 @@
+# Start Docker Compose with .env.prod file
+docker compose --env-file .env.prod up --build
---
+++ docker-up.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Start Docker Compose with .env.prod file
+docker compose --env-file .env.prod up --build
